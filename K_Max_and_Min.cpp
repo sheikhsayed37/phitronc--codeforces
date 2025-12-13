1550714d4fda89f93985fede46b786d6197c20ac
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2025-12-13 21:38:40 +0600

    44

--- K_Max_and_Min.cpp
+++ K_Max_and_Min.cpp
@@ -7,12 +7,14 @@ int main()
     {
         cin>> a[i];
     }
+
     int mx=INT_MIN;   // ata direct akta coto man ace ja nijer modde rakke pore loop e doke count kore
                        // ak ak kore
     for(int i=0;i<3;i++)
     {
         mx=max(a[i],mx);
     }
+    
     int mn=INT_MAX;
 
     for(int i=0;i<3;i++)
