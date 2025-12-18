3601f018b18938e7fd3840fe2c6dcbe8322c15a6
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2025-12-18 18:31:12 +0600

    12

--- practiseweek2er2.cpp
+++ practiseweek2er2.cpp
@@ -10,15 +10,19 @@ using namespace std;
 int main()
 {
      Cricketer *dhoni=new Cricketer[12];
-     dhoni->jersey_no=7;
-     dhoni->country="india";
+     dhoni->jersey_no=7;   // ata direct heap memory gia address ta raktese
+     dhoni->country="india"; //ata direct heap memory gia address ta raktese
+
      Cricketer *kohli=new Cricketer[12];
      kohli->jersey_no=9;
      kohli->country="faidia";
+
        kohli->jersey_no=dhoni->jersey_no;
-     kohli->country=dhoni->country;                       // kohli =dhoni;it doesnt work .and not showing the output
-    delete[] dhoni;
-    cout<<(*kohli).jersey_no<<" "<<(*kohli).country<<" ";
+     kohli->country=dhoni->country;   
+                                     // kohli =dhoni;it doesnt work .and not showing the output
+   
+    cout<<(*kohli).jersey_no<<" "<<(*kohli).country<<" "<<endl;
+    
 
 
 
