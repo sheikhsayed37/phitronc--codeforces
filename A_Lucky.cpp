4d929151314e4060bd4793820376bdfe98f38306
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2026-01-21 21:16:53 +0600

    2

--- A_Lucky.cpp
+++ A_Lucky.cpp
@@ -16,7 +16,7 @@ int main()
        }
       for(int i=3;i<6;i++)
        {
-         sum2= sum2+test[i]-'0';
+         sum2= sum2+test[i]-'0';  
        }
 
 
@@ -28,8 +28,14 @@ int main()
        {
          cout << "NO"<<endl;
        }
-       
-        
+          
     }
-   
-}
+}//akane   -'0' kno disi karon test[i] akane soinka tkleo agola character hisebe kaj kore  
+                                                    //Computer character রাখে ASCII value দিয়ে।
+                                                    // Character	ASCII value
+                                                                // '0'=	48
+                                                                // '1'=	49        like 3 er ketre 51 hole 51-48=3 pawa jai 
+                                                                // '2'=	50 
+                                                                // '3'=	51
+
+                                                             
