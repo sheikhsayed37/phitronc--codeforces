df08001d09b71a63978e1868479f57f81caa0fa7
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2026-01-21 15:32:25 +0600

    125

--- V_Replace_Word.cpp
+++ V_Replace_Word.cpp
@@ -1,30 +1,40 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//int main()
+//{
+// string s;
+//cin>> s;
+// for(int i = 0; i < s.size(); i++)
+// {
+//   s[i] = toupper(s[i]);
+// }
+  
+//for(int i=0;i<s.size()-5;i++)
+//{
+//if(s.substr(i,5)=="EGYPT")  
+//{
+// s.replace(i,5," ");
+//}
+//}
+//cout<<s;
+//}
+
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
    string s;
-   getline(cin, s);
-
-   for(int i=0;i<s.size();i++)
+   cin>> s;
+   for(int i = 0; i < s.size(); i++)
+  {
+   s[i] = toupper(s[i]);
+  }
+   while(s.find("EGYPT")!=string:: npos)
+   
    {
-     char c=towupper(s[i]);
-
+      s.replace(s.find("EGYPT"),5," ");
    } 
-   
-   stringstream ss(s);
-   string word;
-    ss>>word;
-    for(int i=0;i<word.size();i++)
-   {
-        if (word[i]=="EGYPT")
-         {
-            continue;           // এই word গুলো ignore হবে
-            
-         }
-    
-   }
-   cout<< word<<" ";
-   
-
+cout<< s;
 
 }
