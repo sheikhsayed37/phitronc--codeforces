6136dd06c231e7ebec6e91309a53cb5c16e4fc8c
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2026-02-01 23:32:02 +0600

    1

--- G_Even_Hate_Odd.cpp
+++ G_Even_Hate_Odd.cpp
@@ -1,53 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
     int t;
-    
-    cin >> t;
-
-    for(int i = 1; i <= t; i++)
-
+    cin>> t;
+    for(int i=0;i<t;i++)
     {
-        int n;
-        cin >> n;
-
-        int a[n];          // array নেওয়া
-        int even = 0;
-        int odd = 0;
-
-        // array input
-        for(int i = 0; i < n; i++)
+        int x;
+        cin>> x;
+        int a[x];
+        int even=0,odd=0;
+        for(int i=0;i<x;i++)
         {
-            cin >> a[i];
+          cin>> a[i];
         }
-
-        // even-odd count
-        for(int i = 0; i < n; i++)
+        for(int i=0;i<x;i++)
         {
-            if(a[i] % 2 == 0)
-            {
-              even++;  
-            }
-                
-            else
-            {
-                odd++; 
-            }
-                
-        }
+          if(a[i]%2==0)
+          {
+            even++;
+          }
+          else
+          {
+            odd++;
+          } 
 
-        // answer
-        if(n % 2 !=0)
-        {
-            cout << -1 << endl;
         }
-        else
-        {
-            cout << abs(even - odd) / 2 << endl;
-        }
-        }
+       if(x%2==1)
+       {
+           cout<<-1<<endl;
+       }
+       else
+       {
+        cout<<abs(even-odd)/2<<endl;
+       }
 
-    return 0;
+    }
 }
