bbe7cdd9b98ec557a1502fdba2fbca777ad2ce48
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2026-02-02 23:38:48 +0600

    5

---
+++ practiclassseweek2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin,s);
+    
+    string s1="john";
+
+    int  count=0;
+    stringstream ss(s);
+    string word;
+    while(ss>>word)
+    {
+        if(word==s1)
+        {
+        count++;
+        }
+    }
+    cout<<count;
+}
--- practiseclass2.cpp
+++ practiseclass2.cpp
@@ -7,22 +7,23 @@ class student
     int roll;
     int marks;
 };
+
 int main()
 {
    int n;
    cin>> n;
    student a[n];
-   for(int i=0;i<n;i++) // basic coding er motoi
+   for(int i=0;i<n;i++)    // basic coding er motoi
    {
     cin>> a[i].name>>a[i].roll>>a[i].marks;
-
    }
-   reverse(a,a+n);  // reverse kore deyar jonno sobgola 
    
+   reverse(a,a+n);       // reverse kore deyar jonno sobgola 
+
    for(int i=0;i<n;i++)
    {
     cout<< a[i].name<<" "<<a[i].roll<<" "<<a[i].marks<<endl;
-   } 
+   }
 
 }
 
