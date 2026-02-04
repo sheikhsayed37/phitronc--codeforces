21d22c85a4faf8132784c07fe752bacd03101a02
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2026-02-05 00:16:37 +0600

    111

---
+++ I_Smallest_Pair.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+for(int i=0;i<t;i++)
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int minimum=INT_MAX;
+    int value;
+     for(int i=0;i<n;i++)
+     {
+     for(int j=i+1;j<n;j++)
+     {
+        value=a[i]+a[j]+j-i;
+        minimum=min(minimum,value);
+     }
+ }
+cout<<minimum<<endl;
+
+
+}
+}
